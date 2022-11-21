b11a56bca0b4dccc7bfb2bf05632f9b8512fa2d2
Pierre <53476143+RoYaLSleT@users.noreply.github.com>
2022-11-21 15:44:42 +0100

    Algo 6 remaster

--- algo/6/sumArr.js
+++ algo/6/sumArr.js
@@ -13,11 +13,30 @@ sumArr( ["2", "7", "3", "8", "2"], ["2", "4", "9"] ) doit renvoyer ["4", "11", "
 
 function sumArr(arrayA, arrayB) {
 
+  // big contiendra le plus grand des deux tableaux
+  let {big, little} = arrayA.length > arrayB.length ? {big:arrayA, little:arrayB}:{big:arrayB, little:arrayA}
+  // nombre d'element à rajouter à little pour qu'il devienne egal à big
+  const diff = big.length - little.length
+  // on rempli le tableau qui va être ajouté à little par des zeros.
+  const addedElements = new Array(diff).fill(0)
+  // on recréé little à partir de little + addedElements
+  little = [...little, ...addedElements]
+
+  const results = []
+
+  for (let index = 0; index < big.length; index++) {
+    const addition = parseInt(big[index]) + parseInt(little[index])
+    results.push(addition.toString())
+  }
+  
+  return results
+
+  /*
   let {big, little} = arrayA.length > arrayB.length ? {big:arrayA, little:arrayB}: {big:arrayB, little:arrayA}
   little = [...little, ...(new Array(big.length - little.length).fill(0))]
-
   const convertArray = big.map((el, index)=>(parseInt(el) + parseInt(little[index])).toString())
   return convertArray
+  */
 }
 
 module.exports = sumArr;
